cdcfd79787b2a88e9b04e5408b40940f5945b3a9
Marcos Issler <marcos@bosshouse.com.br>
2024-02-23 19:39:21 -0300

    Slice update

--- 03-Fundacao/06-Slice/main.go
+++ 03-Fundacao/06-Slice/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	s := []int{1, 2, 4, 8, 16, 32, 64, 128}
@@ -15,4 +17,76 @@ func main() {
 	s = append(s, 256) // aqui a capacidade do array é dobrada e não incrementada em 1.
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 
+	min, max := MinMax(s)
+	fmt.Printf("Min: %d, Max: %d\n", min, max)
+	fmt.Printf("Min: %d\n", Min(s))
+	fmt.Printf("Max: %d\n", Max(s))
+	fmt.Printf("MinNil: %d\n", Min([]int{}))
+
+}
+
+func MinMax(array []int) (int, int) {
+	if len(array) == 0 {
+		panic("empty array")
+	}
+	var max int = array[0]
+	var min int = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
+
+func Min(array []int) int {
+	if len(array) == 0 {
+		panic("empty array")
+	}
+
+	var min int = array[0]
+	for _, value := range array {
+		if min > value {
+			min = value
+		}
+	}
+	return min
+}
+
+func Max(array []int) int {
+	if len(array) == 0 {
+		panic("empty array")
+	}
+	var max int = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+	}
+	return max
+}
+
+func removeValue(A []int32, v int32) []int32 {
+	var result []int32
+	for i, a := range A {
+		if v == a {
+			return append(A[:i], A[i+1:]...)
+		}
+	}
+	return result
+}
+
+func rotateLeft(d int32, arr []int32) []int32 {
+	if d == 0 || len(arr) == 0 {
+		return arr
+	}
+	shifted := make([]int32, len(arr))
+
+	for i := range arr {
+		shifted[i] = arr[(int32(int32(i)+d))%int32(len(arr))]
+	}
+	return shifted
 }
