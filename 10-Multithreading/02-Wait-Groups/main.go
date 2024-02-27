dce01b66f53c8f3a252ae06ed5754fe43c5475e7
Marcos Issler <marcos@bosshouse.com.br>
2024-02-27 14:27:46 -0300

    Multithreading - Wait-Group updates

--- 10-Multithreading/02-Wait-Groups/main.go
+++ 10-Multithreading/02-Wait-Groups/main.go
@@ -10,26 +10,28 @@ func task(name string, wg *sync.WaitGroup) {
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%d: Task %s is running\n", i, name)
 		time.Sleep(500 * time.Millisecond)
-		wg.Done()
 	}
+	wg.Done()
 }
 
 // Thread 1
 func main() {
 	// Waitgroup here works like a credit. When all credits are used ( .done() ) the main is concluded.
 	waitGroup := sync.WaitGroup{}
-	waitGroup.Add(25)
 	// Thread 2
+	waitGroup.Add(1)
 	go task("A", &waitGroup)
 	// Thread 3
+	waitGroup.Add(1)
 	go task("B", &waitGroup)
 	// Thread 4
 	go func() {
+		waitGroup.Add(1)
 		for i := 0; i < 5; i++ {
 			fmt.Printf("%d: Task %s is running\n", i, "anonymous func")
 			time.Sleep(500 * time.Millisecond)
-			waitGroup.Done()
 		}
+		waitGroup.Done()
 	}()
 	waitGroup.Wait()
 }
