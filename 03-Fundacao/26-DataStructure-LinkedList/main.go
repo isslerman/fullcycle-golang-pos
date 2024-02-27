3fcef4bf1ba5e6b8340a56475b79087d1683d17c
Marcos Issler <marcos@bosshouse.com.br>
2024-02-27 14:27:20 -0300

    DataStructure - LinkedList update

--- 03-Fundacao/26-DataStructure-LinkedList/main.go
+++ 03-Fundacao/26-DataStructure-LinkedList/main.go
@@ -27,6 +27,27 @@ func (l *linkedList) print() {
 	}
 }
 
+// DeleteNode deletes a node with the given value from the linked list
+func (l *linkedList) DeleteNode(value int) {
+	if l.head == nil {
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		return
+	}
+
+	current := l.head
+	for current.next != nil && current.next.data != value {
+		current = current.next
+	}
+
+	if current.next != nil {
+		current.next = current.next.next
+	}
+}
+
 func main() {
 	myList := linkedList{}
 	node1 := &node{data: 10}
